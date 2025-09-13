bd6f13440e67e83ac4f69afe70ed083b7bf2be49
yohnx <yohanneskefelegnn@gmail.com>
2025-09-12 21:39:15 -0700

    .

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -4,7 +4,7 @@ import SectionTitle from "./SectionTitle";
 
 const Projects = () => {
   return (
-    <section className="py-20 mx-auto max-w-7xl" id="projects">
+    <section className="py-20 mx-auto max-w-7xl px-8" id="projects">
       <SectionTitle text="Web section" />
       <div className="py-16 grid lg:grid-cols-2 xl:grid-cols-3 gap-8">
         {projects.map((project) => {
--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -5,7 +5,7 @@ import SkillsCard from "./SkillsCard";
 
 const Skills = () => {
   return (
-    <section className="mx-auto max-w-7xl py-20" id="skills">
+    <section className="mx-auto max-w-7xl py-20 px-8" id="skills">
       <SectionTitle text="tech stack" />
       <div className="py-16 grid gap-8 md:grid-cols-2 lg:grid-cols-3">
         {skills.map((skill) => {
